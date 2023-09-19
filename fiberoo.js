aaf0c7e15a4164dd417689090950ddabaab87565
Josiah Liu <josiahsk@hawaii.edu>
2023-09-19 12:58:18 -1000

    practice WOD finished

---
+++ fiberoo.js
@@ -0,0 +1,39 @@
+
+function fib() {
+  var fibArray = [0, 1];
+  for (let i = 2; i < 50; i++) {
+    fibArray.push(fibArray[i - 1] + fibArray[i - 2]);
+  }
+  return fibArray;
+}
+
+console.log(fib());
+
+function numsToString (numbers) {
+  return _.map(numbers, function (num) { return `${num}`; });
+}
+
+console.log(numsToString(fib()));
+
+function numEvenNums (numbers) {
+  return _.filter(numbers, function (num) {return num % 2 ===0;})
+}
+console.log(numEvenNums(fib()));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
